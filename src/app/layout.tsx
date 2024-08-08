f65b3a2149fcb2adfa1e5aee90d63207c529a211
yassinehaimouch <haymouch53@gmail.com>
2024-08-08 13:31:34 +0100

    [Add] OG-Image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,17 @@ import "./globals.css";
 const inter = Sora({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://bee.gitcoder.me"),
   title: "Spelling Bee",
-  description: "Spelling Bee By Yassine Haimouch (@gitcoder).",
+  description: "Developer, writer, and creator.",
+  openGraph: {
+    title: "Yassine Haimouch",
+    description: "Spelling Bee By Yassine Haimouch (@gitcoder).",
+    url: "https://bee.gitcoder.me",
+    siteName: "Spelling Bee",
+    locale: "en_US",
+    type: "website",
+  }
 };
 
 export default function RootLayout({
